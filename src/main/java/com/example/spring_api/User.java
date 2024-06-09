7c553f831d37b015da13796f757304c62b649857
Gerges Ghaly <82944591+GergesGhaly@users.noreply.github.com>
2024-06-09 05:33:13 +0300

    Update User.java

--- src/main/java/com/example/spring_api/User.java
+++ src/main/java/com/example/spring_api/User.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     
     @Column
